0e3523ad2179e7ea4f2e460eceb8f9201eab7955
ramonclf <ramonclf@hotmail.com>
2025-09-03 15:50:45 -0300

    Create renderPainel.js

---
+++ 4. scripts/5. scripts/6. scripts/renderPainel.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  const container = document.getElementById("painel-container");
+  container.innerHTML = "<p>Renderizando painéis conforme perfil...</p>";
+  // Aqui você pode integrar com Firebase para buscar dados e renderizar os painéis reais
+};
