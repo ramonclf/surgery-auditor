b1cf475054a6cb907415c8af802ac3722580d6a4
ramonclf <ramonclf@hotmail.com>
2025-09-03 17:50:12 -0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC9U8jDIr_TAP0CLDAidKyThhOyf0DLR7E",
+  authDomain: "surgery-auditor.firebaseapp.com",
+  projectId: "surgery-auditor",
+  storageBucket: "surgery-auditor.firebasestorage.app",
+  messagingSenderId: "534090644906",
+  appId: "1:534090644906:web:875670b0a7fbfa685a7fd3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
