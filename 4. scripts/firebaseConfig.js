a3fabf0dc9d52f3e4861044f57377fd14d133938
ramonclf <ramonclf@hotmail.com>
2025-09-03 19:01:40 -0300

    Update firebaseConfig.js

--- 4. scripts/firebaseConfig.js
+++ 4. scripts/firebaseConfig.js
@@ -1,11 +1,19 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyC9U8jDIr_TAP0CLDAidKyThhOyf0DLR7E",
-  authDomain: "surgery-auditor.firebaseapp.com",
-  projectId: "surgery-auditor",
-  storageBucket: "surgery-auditor.firebasestorage.app",
-  messagingSenderId: "534090644906",
-  appId: "1:534090644906:web:875670b0a7fbfa685a7fd3"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC9U8jDIr_TAP0CLDAidKyThhOyf0DLR7E",
+    authDomain: "surgery-auditor.firebaseapp.com",
+    projectId: "surgery-auditor",
+    storageBucket: "surgery-auditor.firebasestorage.app",
+    messagingSenderId: "534090644906",
+    appId: "1:534090644906:web:875670b0a7fbfa685a7fd3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
