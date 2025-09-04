47cbc6f23af30737c9b721bfc2493c2cc0ab50cb
ramonclf <ramonclf@hotmail.com>
2025-09-03 22:34:29 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,10 +3,16 @@ function login() {
   const password = document.getElementById("password").value;
 
   firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
+    .then(() => {
       window.location.href = "painel.html";
     })
     .catch((error) => {
       alert("Erro ao fazer login: " + error.message);
     });
+}
+
+function logout() {
+  firebase.auth().signOut().then(() => {
+    window.location.href = "login.html";
+  });
 }
