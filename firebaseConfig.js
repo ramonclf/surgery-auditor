19aa74c46f70bbd252f40473860be6e9bbc2197d
ramonclf <ramonclf@hotmail.com>
2025-09-03 22:05:47 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,3 +1,7 @@
+<script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-auth-compat.js"></script>
+<script src="firebaseConfig.js"></script>
+
 const firebaseConfig = {
   apiKey: "AIzaSyC9U8jDIr_TAP0CLDAidKyThhOyf0DLR7E",
   authDomain: "surgery-auditor.firebaseapp.com",
@@ -7,5 +11,4 @@ const firebaseConfig = {
   appId: "1:534090644906:web:875670b0a7fbfa685a7fd3"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
